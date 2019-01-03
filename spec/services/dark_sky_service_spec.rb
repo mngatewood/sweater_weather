9a3948ed252e112371df1d5f8f20a1fe6b58a4ae
Michael Gatewood <michael@mngatewood.com>
2019-01-03 15:06:28 -0700

    Adds vcr to services specs

--- spec/services/dark_sky_service_spec.rb
+++ spec/services/dark_sky_service_spec.rb
@@ -2,18 +2,20 @@ require 'rails_helper'
 
 describe 'DarkSkyService' do
   it 'should return a forcast' do
-    latitude = 39.7507834
-    longitude = -104.9964355
-    service = DarkSkyService.new(latitude, longitude)
-    expected_keys = [:latitude,
-                :longitude,
-                :timezone,
-                :currently,
-                :minutely,
-                :hourly,
-                :daily,
-                :flags,
-                :offset]
-    expect(service.forecast.keys).to eq(expected_keys)
+    VCR.use_cassette('dark_sky_request', record: :all) do
+      latitude = 39.7507834
+      longitude = -104.9964355
+      service = DarkSkyService.new(latitude, longitude)
+      expected_keys =  [:latitude,
+                        :longitude,
+                        :timezone,
+                        :currently,
+                        :minutely,
+                        :hourly,
+                        :daily,
+                        :flags,
+                        :offset]
+      expect(service.forecast.keys).to eq(expected_keys)
+    end
   end
 end
--- spec/services/google_geocode_service_spec.rb
+++ spec/services/google_geocode_service_spec.rb
@@ -2,18 +2,23 @@ require 'rails_helper'
 
 describe 'GoogleGeocodeService' do
 
+  before(:each) do
+    location = "Denver, CO"
+    @service = GoogleGeocodeService.new(location)
+  end
+
   it 'should return coordinates' do
-    address = "1331 17th Street, Denver, CO 80202"
-    service = GoogleGeocodeService.new(address)
-    expect(service.coordinates).to eq({lat: 39.7507834,
-                                       lng: -104.9964355})
+    VCR.use_cassette('geocode_lookup', record: :all) do
+      expect(@service.coordinates).to eq({lat: 39.7392358,
+                                          lng: -104.990251})
+    end
   end
 
   it 'should return location data (city, state, and country)' do
-    address = "1331 17th Street, Denver, CO 80202"
-    service = GoogleGeocodeService.new(address)
-    expected = JSON.parse(File.read("./spec/fixtures/location_data.json"), symbolize_names: true)
-    expect(service.location_data).to eq(expected)
+    VCR.use_cassette('geocode_lookup', record: :all) do
+      expected = JSON.parse(File.read("./spec/fixtures/location_data.json"), symbolize_names: true)
+      expect(@service.location_data).to eq(expected)
+    end
   end
 
 end
