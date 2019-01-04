e6460f31dbaf626365f7f95d9e2c1c83bb7420b4
Michael Gatewood <michael@mngatewood.com>
2019-01-04 11:15:10 -0700

    Moves gif fetch to service

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -7,24 +7,15 @@ class Gif
     @time = time
   end
 
-  def data
-    url = "search?api_key=#{ENV['GIPHY_API_KEY']}&q=#{summary}"
-    data = get_json(url)
-  end
-
   def url
+    data = giphy_service.gif_fetch
     data[:data].first[:url]
   end
 
 private
 
-  def get_json(url)
-    response = conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def conn
-    Faraday.new(url: "http://api.giphy.com/v1/gifs/")
+  def giphy_service
+    GiphyService.new(summary)
   end
 
 end
