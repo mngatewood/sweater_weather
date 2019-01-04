114dcd0d19397a11c9d5d8c91700d12ea501c511
Michael Gatewood <michael@mngatewood.com>
2019-01-04 12:06:11 -0700

    Adds giphy api key filter to VCR config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,7 @@ VCR.configure do |config|
   config.configure_rspec_metadata!
   config.filter_sensitive_data("<GOOGLE_API_KEY>") { ENV['GOOGLE_API_KEY'] }
   config.filter_sensitive_data("<DARK_SKY_API_KEY>") { ENV['DARK_SKY_API_KEY'] }
+  config.filter_sensitive_data("<GIPHY_API_KEY>") { ENV['GIPHY_API_KEY'] }
 end
 
 # Add additional requires below this line. Rails is not loaded until this point!
