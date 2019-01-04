8df6d3dec732f17dbbd422f6a29943275463b054
Michael Gatewood <michael@mngatewood.com>
2019-01-04 09:42:06 -0700

    Adds gifs route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
         get 'forecast', to: 'forecast#show'
         post 'users', to: 'users#create'
+        get 'gifs', to: 'gifs#show'
     end
   end
 
