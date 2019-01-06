0e9728900be2af204c27a6927a1da96ac1f1651e
Michael Gatewood <michael@mngatewood.com>
2019-01-05 17:48:08 -0700

    Adds route for sessions#create

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
         get 'forecast', to: 'forecast#show'
         post 'users', to: 'users#create'
         get 'gifs', to: 'gifs#show'
+        post 'sessions', to: 'sessions#create'
     end
   end
 
