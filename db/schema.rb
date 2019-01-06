06671f947ec1466372ad513c35fc8e910bb5f1e8
Michael Gatewood <michael@mngatewood.com>
2019-01-05 20:05:04 -0700

    Adds favorites to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_03_225327) do
+ActiveRecord::Schema.define(version: 2019_01_06_021749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "location"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -23,4 +29,5 @@ ActiveRecord::Schema.define(version: 2019_01_03_225327) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "favorites", "users"
 end
